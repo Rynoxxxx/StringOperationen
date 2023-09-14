434086bf686978b37e440759c37b744611306385
samuel.stroot <samuel.stroot@gmail.com>
2023-09-14 09:46:22 +0200

    operationen 14.09

---
+++ src/StringOperationenGUI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class StringOperationenGUI {
+    private JPanel mainPanel;
+    private JButton charAtButton;
+    private JButton substringButton;
+    private JButton lengthButton;
+    private JButton upperCaseButton;
+    private JButton lowerCaseButton;
+    private JButton indexOfButton;
+    private JButton equalsButton;
+    private JButton button8;
+    private JButton lastIndexOfButton;
+    private JTextField ausgabeFeld;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField stringEingabe;
+}
